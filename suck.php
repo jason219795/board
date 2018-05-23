ad7f0ef19f0fea007ada57e7bf3981e60c62d981
jason219795 <39554484+jason219795@users.noreply.github.com>
2018-05-23 18:16:43 +0800

    Add files via upload

---
+++ suck.php
@@ -0,0 +1,27 @@
+﻿<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "board";
+$userid=$_POST['userid'];
+$psw=$_POST['psw'];
+
+// Create connection
+
+$conn = new mysqli($servername ,$user, $password,$dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO account (username, password)
+VALUES ('$userid', '$psw')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "帳號註冊成功";
+} else {
+    echo "失敗: 帳號 $userid 已有人註冊" ;
+}
+
+$conn->close();
+?>
